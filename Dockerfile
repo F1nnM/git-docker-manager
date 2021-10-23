b894aa634f01b371b752909a99543e7cae410f22
Finn Melzer <github@mfinn.de>
2021-10-23 17:10:21 +0200

    Fixed it

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
-FROM node:lts-alpine
+FROM node:lts
 
 WORKDIR /app
 
-COPY . .
+RUN curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+RUN chmod +x /usr/local/bin/docker-compose
+
+COPY ./package.json .
+COPY ./package-lock.json .
 
 RUN npm install
+
+COPY . .
 
 EXPOSE 5000
 
--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 var express = require('express')
-const {hook} = require('express-git-hook')
+const { hook } = require('express-git-hook')
+const { exec, execFile } = require('child_process');
+const { stdout } = require('process');
 
 const mw = hook(process.env.WATCHED_REPO, "./cloned-repo")
 
 var app = express()
 
 app.post('/', mw, (req, res) => {
-    // res.locals.files contains a list of all files with their full paths
+  // res.locals.files contains a list of all files with their full paths
 
-    // async wrapper so errors don't interupt anything else
-    (async () => {
-      const toCompose = res.locals.files["compose"]
-      if( toCompose ){
-        if ( typeof toCompose === "string")
-          throw new Error("./compose is not a directory!")
-  
-        for (const project of toCompose)
-          // async wrapper so errors don't interrupt other deployments
-          (async (project) => {
-            if (typeof project === "string")
-              throw new Error(`${project} is not a directory!`)
-            
-            exec('docker-compose up -d --remove-orphans', {cwd: project}, (error, stdout, stderr) => {
-              if(err)
-                throw new Error(stderr)
-              else console.log(stdout)
-            })
+  // async wrapper so errors don't interupt anything else
+  (async () => {
+    const toCompose = res.locals.files["compose"]
+    if (toCompose) {
+      if (typeof toCompose === "string")
+        throw new Error("./compose is not a directory!")
 
-          })()
-      }
-    })()
+      for (const project in toCompose)
+        // async wrapper so errors don't interrupt other deployments
+        (async (project) => {
+          if (typeof toCompose[project] === "string")
+            throw new Error(`${project} is not a directory!`)
+
+          execFile('/usr/local/bin/docker-compose', ['-f', toCompose[project]['docker-compose.yml'], 'up', '-d', '--remove-orphans'], async (error, stdout, stderr) => {
+            if (error) {
+              console.error(stderr)
+              throw error
+            }
+            else console.log(stdout)
+          })
+
+        })(project)
+    }
+  })()
 })
 
 app.listen(5000, "0.0.0.0", () => {
