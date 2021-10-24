c5c30787179d17a75d74eb2a5e975377fb52d910
Finn Melzer <github@mfinn.de>
2021-10-24 20:26:28 +0200

    Fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var app = express()
 
 app.post('/', mw, (req, res) => {
   // res.locals.files contains a list of all files with their full paths
-  deploy(res.locals.files["compose"], res.status(200).end)
+  deploy(res.locals.files["compose"], () => res.status(200).end())
 })
 
 app.listen(5000, "0.0.0.0", () => {
