cb985fe7a3bf1ec19d4d78341699bec624a89d24
Finn Melzer <github@mfinn.de>
2021-11-18 16:04:06 +0100

    Fixed cloning of private repos

--- init.js
+++ init.js
@@ -24,6 +24,17 @@ const clonePath = "cloned-repo";
 
 (async () => {
   del.sync([clonePath+"/**", clonePath+"/.**", "!"+clonePath])
+
+  let cloneUrl
+  const pat = process.env.PAT
+  if(pat){
+    cloneUrl = `https://user:${pat}@github.com/${data.repository.full_name}`
+  }else{
+    if(data.repository.private)
+      throw new Error("Received hook from private repository, but no PAT was supplied!")
+      cloneUrl = `https://github.com/${data.repository.full_name}`
+  }
+  
   try {
     await clone("https://github.com/" + process.env.REPOSITORY, clonePath, {args: ["--recursive", "-j8"]})
   } catch (e) {
